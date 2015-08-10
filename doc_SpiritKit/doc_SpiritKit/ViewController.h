3ca6418c989c69a29001ce3f5014382755cfd601
nfhy <nfhy@wangluludeMacBook-Air.local>
2015-08-10 16:58:47 +0800

    init

---
+++ doc_SpiritKit/doc_SpiritKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  doc_SpiritKit
+//
+//  Created by 王露露 on 15/8/10.
+//  Copyright (c) 2015年 王露露. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ doc_SpiritKit/doc_SpiritKit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  doc_SpiritKit
+//
+//  Created by 王露露 on 15/8/10.
+//  Copyright (c) 2015年 王露露. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
